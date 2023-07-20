b188c1d1b132467e60e8fbb2ca50a11f55cc2b68
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-07-20 22:47:35 +0530

    bug fix

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,23 @@
-import PropTypes from 'prop-types'
-import { useLocation } from 'react-router-dom'
+import propTypes from 'prop-types'
 import Button from './Button'
-
-const Header = ({ title, onAdd, showAdd }) => {
-  const location = useLocation()
-
+const header = ({title,onToggleAdd, showAdd}) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      {location.pathname === '/' && (
-        <Button
-          color={showAdd ? 'red' : 'green'}
-          text={showAdd ? 'Close' : 'Add'}
-          onClick={onAdd}
-        />
-      )}
+      <Button text={showAdd ? 'Close':'Add'} color ={showAdd ? 'red':'green'} onClick={onToggleAdd}/>
+
     </header>
   )
 }
-
-Header.defaultProps = {
-  title: 'Task Tracker',
-}
-
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-}
-
-// CSS in JS
-// const headingStyle = {
-//   color: 'red',
-//   backgroundColor: 'black',
-// }
-
-export default Header
 
 // header.defaultProps={
 //     title: 'Hello',
 // }
 // It will put title to Hello as default
 
-
+header.propTypes = {
+  title: propTypes.string.isRequired,
+}
 
 // We Can use internal Css
 /* <h1 style={{color:'red',backgroundColor:'black'}}>{props.title}</h1> */
@@ -51,3 +28,5 @@ export default Header
 //   backgroundColor: 'black'
 // }
 // <h1 style={stylehead}>{props.title}</h1>
+
+export default header
